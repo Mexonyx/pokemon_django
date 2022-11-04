b9eee1d7281df100c5eb34f1b4e975cde9279ad7
unknown <glourenco16@outlook.fr>
2022-11-04 15:50:37 +0100

    ajout de la recupération de pokemon et création d'une vue

--- PokedexProject/pokedex/views.py
+++ PokedexProject/pokedex/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import requests
 # Create your views here.
+
+def pokedex(request):
+    url = "https://pokeapi.co/api/v2/pokemon/?limit=151"
+    r = requests.get(url)
+    print(r)
+    jsonRequest = r.json()
+    listpokemon = []
+    for pokemon in jsonRequest['results']:
+        listpokemon.append(pokemon['name'] + ' ')
+
+    return HttpResponse(listpokemon)
