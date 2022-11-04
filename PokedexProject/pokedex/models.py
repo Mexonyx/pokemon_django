27366657516370540c12950f7da4ae6824fde9a6
unknown <glourenco16@outlook.fr>
2022-11-04 17:03:10 +0100

    affichage des pokemons en liste

--- PokedexProject/pokedex/models.py
+++ PokedexProject/pokedex/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Pokemon(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    urlImage = models.CharField(max_length=500)
+    type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return  str(self.id) + " " + self.name + " " + self.type + "\n"
--- PokedexProject/pokedex/views.py
+++ PokedexProject/pokedex/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pokemon
 import requests
 # Create your views here.
 
@@ -9,7 +10,25 @@ def pokedex(request):
     print(r)
     jsonRequest = r.json()
     listpokemon = []
-    for pokemon in jsonRequest['results']:
-        listpokemon.append(pokemon['name'] + ' ')
+    
+    for aPokemon in jsonRequest['results']:
+        urlDetailPokemon = aPokemon['url']
+        result = requests.get(urlDetailPokemon)
+        detailPokemonJson = result.json()
 
-    return HttpResponse(listpokemon)
+        
+        typesPokemon = ""
+        # if properties == "types":
+        #     for type in properties['types']:
+        #         typesPokemon += type['type']['name']
+        # if properties == "id":
+        #     idPokemon = properties['id']
+        # if properties == "sprites":
+        #     imageDetailPokemon = properties['sprites']['other']['official-artwork']['front_default']
+        # if properties == "types":
+        #     typeDetailPokemon = properties['types']
+        listpokemon.append(Pokemon(id=detailPokemonJson['id'], name = detailPokemonJson['name'],urlImage = detailPokemonJson['sprites']['other']['official-artwork']['front_default'], type = detailPokemonJson['types'][0]['type']['name']))
+
+    context = {"pokemonList" : listpokemon} 
+
+    return render(request,'pokedex/index.html', context)
